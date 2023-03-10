572a0ba82d371b61611bbf7f3d78e32b68800302
Jeremy SPAETH <ak85ie1d5@relay.firefox.com>
2023-03-10 10:18:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Theta Snippet
+
+Large set of code snippet to extends and enhanced Agence Theta website.
---
+++ theta-theme-snippet.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Theta Theme Snippet
+ *
+ * @package           Theta\ThemeSnippet
+ * @author            Jeremy SPAETH
+ * @copyright         2023 Theta
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Theta Theme Snippet
+ * Plugin URI:        https://github.com/ak85ie1d5/theta-theme-snippet
+ * Description:       Large set of code snippet to extends and enhanced Agence Theta website.
+ * Version:           0.1
+ * Requires at least: 6.1.0
+ * Requires PHP:      8.0
+ * Author:            Jeremy SPAETH
+ * Author URI:        https://github.com/ak85ie1d5
+ * Domain Path:       /i18n
+ * Text Domain:       theta-theme-snippet
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
